45be56be222bf01c363a0edf133f0968005b073a
SABNETCOMMS_LTD\sabih <sabih.abbbasi@yahoo.com>
2026-08-24 13:14:41 +0100

    Test CI/CD pipeline

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int main() {
     printf("Calculator Program\n");
     printf("5 + 3 = %d\n", add(5, 3));
     printf("10 - 4 = %d\n", subtract(10, 4));
+    printf("CI/CD Pipeline Working!\n");
     return 0;
 }
