ce6ba268b1938d4edbf865ccb12fa8c4a8fc6300
sabih1982 <68585848+sabih1982@users.noreply.github.com>
2026-08-25 13:40:10 +0100

    multiply added

--- calculator.c
+++ calculator.c
@@ -7,3 +7,7 @@ int add(int a, int b) {
 int subtract(int a, int b) {
     return a - b;
 }
+
+int multiply(int a, int b){
+    return a * b;
+}
--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int main() {
     printf("Calculator Program\n");
     printf("5 + 3 = %d\n", add(5, 3));
     printf("10 - 4 = %d\n", subtract(10, 4));
+    printf("5 * 4 = %d\n", multiply(10, 4));
     printf("CI/CD Pipeline Working!\n");
     return 0;
 }
